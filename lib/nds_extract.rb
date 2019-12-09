32500f9a75d2f7152ff700bc291203d43166be49
Dzhoy Zuckerman <dzhoyz@gmail.com>
2019-12-09 01:04:07 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -33,27 +33,3 @@ def gross_for_director(director_data)
   end
   total
 end
-
-def directors_totals(source)
-  result = {}
-  director_index = 0
-  while director_index < source.size do
-    director = source[director_index]
-    result[director[:name]] = gross_for_director(director)
-    director_index += 1
-  end
-  result
-end
-
-def gross_for_director(d)
-  total = 0
-  index = 0
-
-  while index < d[:movies].length do
-    total += d[:movies][index][:worldwide_gross]
-    index += 1
-  end
-  total
-end
-
-
